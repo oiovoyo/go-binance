3f1fb461302388b2e7cabb532b9ae49cd373bb1b
george <jqnk124@gmail.com>
2018-02-10 17:48:01 +0800

    www -> api

--- client.go
+++ client.go
@@ -61,7 +61,7 @@ func NewClient(apiKey, secretKey string) *Client {
 	return &Client{
 		APIKey:     apiKey,
 		SecretKey:  secretKey,
-		BaseURL:    "https://www.binance.com",
+		BaseURL:    "https://api.binance.com",
 		UserAgent:  "Binance/golang",
 		HTTPClient: http.DefaultClient,
 		Logger:     log.New(os.Stderr, "Binance-golang ", log.LstdFlags),
